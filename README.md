3d9df5e6e89389123c450a6a0f0cdc335b8d39b2
krmanski <choromanski117@gmail.com>
2022-10-07 23:14:31 -0400

    All buttons pass IDs to switch block
    All buttons pass their IDs into the same function, and then that
    function evaluates the ID via switchblock. The modify grid button
    now returns messages to the webpage and takes input from user on
    click.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Etch-a-Sketch
 A project part of the Odin Project curriculum
+
+Future planned features:
+Sliding scales to select grid size
+Prettier buttons
+
--- main.js
+++ main.js
@@ -1,11 +1,75 @@
+//wait until the HTML and CSS are loaded before you run the javascript
+document.addEventListener("DOMContentLoaded", function(){
+    generateCanvas(32);
+    console.log("dom content is loaded");
+    
+});
+
+function generateCanvas(gridSize){
+    //create the gridlines
+    //divide canvas into A x A where A = the number provided by user
+    let canvas = document.querySelector(".main-grid-container");
+    canvas.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    alert("canvas should be generated");
+
+    let divsTotal = (gridSize * gridSize);
+    for(let i = 0; i < divsTotal; i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor="blue";
+        
+        canvas.insertAdjacentElement("beforeend", div);
+    }
+}
+
+function selectSize(){
+    //has the user type a number and returns that number, but only if it is between 0 and 100
+    let userInput = prompt("What should be the size of the board?")
+    let message = document.querySelector("#message")
+    alert(message);
+    if (!Number.isInteger(userInput)) alert("ERROR, NaN");
+    if (userInput == ""){
+        message.innerText = "Please provide a number only";
+    } else if(userInput < 0 || userInput > 100){
+        message.innerText ="Please only provide a number between 1 and 100"
+    } else {
+        message.innerText =`You selected a ${userInput} x ${userInput} sized grid for your canvas`
+        return userInput;
+    } 
+
+}
+
+
+
+
+
 
 const buttonPress = e =>{
     //takes in the id of the button that was pressed.
     let buttonID = e.target.id;
     console.log(buttonID);
-    alert(`button ${buttonID} was pressed`)
+    alert(`button ${buttonID} was pressed`);
+    switch(buttonID){
+        case "modify-grid":
+            selectSize();
+            break;
+        
+        case "black":
+            alert('you pressed black');
+            break;
+
+        case "red":
+            alert('you pressed red');
+            break;
+        
+        case "reset":
+            alert('you pressed reset');
+            break;
+        
+    }
 }
 
 //get all buttons and make them listen for a click to run a function
 let buttons = document.querySelectorAll(".button");
 buttons.forEach(button => button.addEventListener("click", buttonPress));
+
