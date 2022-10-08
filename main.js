36b7bd64b432f5a35176a53eb9dac739a03253b1
krmanski <choromanski117@gmail.com>
2022-10-07 21:49:57 -0400

    Buttons sent IDs to function
    All buttons are mapped to the same function and they send their id
    as an arg. This was not working at first because I was actually
    clickin on the span element child and not the actual button.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 
-function buttonPress(buttonID){
+const buttonPress = e =>{
+    //takes in the id of the button that was pressed.
+    let buttonID = e.target.id;
+    console.log(buttonID);
     alert(`button ${buttonID} was pressed`)
 }
 
-
+//get all buttons and make them listen for a click to run a function
 let buttons = document.querySelectorAll(".button");
 buttons.forEach(button => button.addEventListener("click", buttonPress));
