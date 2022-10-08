7dd8d64c98ce7f9aaef326d2ba4218d337d9b8b4
krmanski <choromanski117@gmail.com>
2022-10-07 20:52:41 -0400

    Make buttons and run a function
    All buttons are able to call the same function when pressed. Next
    this function needs to be able to take the button ID as an argument.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("apples");
+
+function buttonPress(buttonID){
+    alert(`button ${buttonID} was pressed`)
+}
+
+
+let buttons = document.querySelectorAll(".button");
+buttons.forEach(button => button.addEventListener("click", buttonPress));
