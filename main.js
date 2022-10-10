9cce8fc0e026840a2676a0583d229d5e61279439
krmanski <choromanski117@gmail.com>
2022-10-10 17:22:20 -0400

    Generate grid successfully
    A grid of divs is successfully generated via generateGrid() taking
    user input via promptSize() and sending the information to the
    CSS var --rows and --columns.

--- main.js
+++ main.js
@@ -7,16 +7,38 @@ let currentColor = "black";
 function promptSize() {
     let number = prompt("Choose a grid size between 1-75 for your sketchpad", 16); 
      if (number >= 1 && number <= 75) {
-        alert(`${number}`)
+        generateGrid (number);
      }else {
          do {number = prompt("Invalid input. Provide a number between 1-75 plese.");
          }
          while(number < 1 || number > 75);
          //this loop makes it so that the user will be trapped with further prompts until they provide one in an acceptable range. 
-         alert(`${number}`)
+         generateGrid (number);
 
      }
  }
+
+ function generateGrid(gridSize) {
+    //sends data from function call to the variables set up in the CSS root
+    //find the container ID and modify the --rows property and set it to the number of rows we want to make
+    //once they take our arg to the CSS var, the actual column and rows are generated in CSS.
+    
+    container.style.setProperty('--rows', gridSize); 
+    container.style.setProperty('--columns', gridSize); 
+    for (let i = 0; i < gridSize; i++) {
+        makeSquares(gridSize);
+    }
+}
+
+
+function makeSquares (squareNumber) {
+    for (let i = 0; i < squareNumber; i++) {
+        square = document.createElement('div');
+        //adding this square class to each div will make it targetable by changeColor and will allow us to style them with CSS before they even exist. 
+        square.classList.add('square');
+        container.appendChild(square);
+    }
+}
 
 
 
